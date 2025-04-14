fb5174d2b153c76fe5335ba6a4a635b80a186c95
Felipe-CF <prog.study.fcf@gmail.com>
2025-04-14 12:26:32 -0300

    add: .md para registro de desempenho e primeiro teste com lgbm

--- desafios/dados_base/data_d1.py
+++ desafios/dados_base/data_d1.py
@@ -13,3 +13,16 @@ dados_frame = pd.DataFrame.copy(dados)
 
 
 dados_frame = dados_frame.loc[:, ~dados_frame.columns.str.contains('^Unnamed')]
+
+dados_frame['diagnosis'].replace(
+    {
+        'M': 1,
+        'B': 0
+    }, inplace=True
+)
+
+alvo = dados_frame.iloc[:, 1]
+
+previsores = dados_frame.iloc[:, 2:32]
+
+
--- desafios/desafio_1.py
+++ desafios/desafio_1.py
@@ -1,10 +1,73 @@
-from dados_base.data_d1 import *
 import pandas as pd
+import lightgbm as lgb
+from sklearn.svm import SVC
+from datetime import datetime
+from dados_base.data_d1 import *
+from xgboost import XGBClassifier
+from catboost import CatBoostClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import KFold, cross_val_score, train_test_split
+from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
-# c2 = dados_frame.iloc[:, 2].values
-# print(c2.mean())
-print(dados_frame.head())
 
-previsores = dados_frame.iloc[:, 2:32]
+x_train, x_test, y_train, y_test = train_test_split(previsores, alvo, test_size=0.3, random_state=0)
 
-# print(dados_frame.isna().sum())
+
+lgb_dataset = lgb.Dataset(x_train, label=y_train)
+
+parametros = {
+    'num_leaves': 200, 
+    'objective':'binary',
+    'max_depth': 4,
+    'learning_rate': 0.9,
+    'max_bin': 200
+}
+
+lgbm = lgb.train(params=parametros, train_set=lgb_dataset, num_boost_round=150)
+
+previsoes_lgbm = (lgbm.predict(x_test) > 0.5).astype(int)
+
+print("   ===  Previsões de teste ===  ") 
+acuracia = accuracy_score(y_test, previsoes_lgbm) # acuracia da previsão
+
+print(f"Acurácia: {(acuracia*100):.2f}%  \n")
+
+matriz_confusao = confusion_matrix(y_test, previsoes_lgbm) # Matriz de confusão (onde a diagonal principal representa os acertos [0][0] e [1][1])
+
+print(f"Matriz de confusao \n {matriz_confusao} \n")
+
+report_classificacao = classification_report(y_test, previsoes_lgbm)
+
+print(f"Classificação report \n {report_classificacao} \n")
+
+
+print("   ===  Previsões de treino ===  ") 
+previsoes_lgbm = (lgbm.predict(x_train) > 0.5).astype(int)
+
+acuracia = accuracy_score(y_train, previsoes_lgbm) # acuracia da previsão
+
+print(f"Acurácia: {(acuracia*100):.2f}%  \n")
+
+matriz_confusao = confusion_matrix(y_train, previsoes_lgbm) # Matriz de confusão (onde a diagonal principal representa os acertos [0][0] e [1][1])
+
+print(f"Matriz de confusao \n {matriz_confusao} \n")
+
+report_classificacao = classification_report(y_train, previsoes_lgbm)
+
+print(f"Classificação report \n {report_classificacao} \n")
+
+
+
+kfold = KFold(n_splits=30, shuffle=True, random_state=5) # separando os dados em grupos
+
+modelo = lgb.LGBMClassifier(num_leaves=200, objective= 'binary', max_depth=4,
+                            learning_rate=  0.09, max_bin=200)
+
+resultado = cross_val_score(modelo, previsores, alvo, cv=kfold)
+
+print(f"Acurácia da Validação Cruzada: {(resultado.mean()*100):.2f}%  \n")
