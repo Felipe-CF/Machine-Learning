1fcf47b5dd5dcdfa20e95a7bb6439b27704e609f
felipeCosta <felipecosta@processoagil.com.br>
2025-05-20 17:26:57 -0300

    adição da função de backpropagation

--- algoritmos/caligrafia.py
+++ algoritmos/caligrafia.py
@@ -86,8 +86,49 @@ class Network(object):
         self.biases = [b-(eta/len(mini_batch))*nb for b, nb in zip(self.biases, nabla_b)]
 
 
-    def backprop():
-        pass    
+    def backprop(self, x, y):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]    
+
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+
+        # feedforward
+        activation = x
+
+        # lista para armazenar todas as ativações, camada por camada
+        activations = [x]
+
+        # lista para armazenar todas os vetores z, camada por camada
+        zs = []
+
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation)+b
+            
+            zs.append(z)
+
+            activation = sigmoid(z)
+
+            activations.append(activation)
+
+        delta = self.cost_derivative(activations[-1], y) * sigmoid_prime(zs[-1])
+        
+        nabla_b = delta
+        
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+
+        for l in range(2, self.num_layers):
+            z= zs[-l]
+
+            sp = sigmoid_prime(z)
+
+            delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
+
+            nabla_b[-l] = delta
+
+            nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
+
+        return (nabla_b, nabla_w)
+
+
 
         
 rede = Network([2, 3, 1])
