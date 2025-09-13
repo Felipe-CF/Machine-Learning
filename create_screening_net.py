c8628c12cbb8b90f5a0647b7d07be6e0ca755f91
Felipe-CF <prog.study.fcf@gmail.com>
2025-09-12 22:23:06 -0300

    feat: inicio de treino da ResNet11

--- create_screening_net.py
+++ create_screening_net.py
@@ -29,6 +29,8 @@ def create_load_net(file_dir=None):
 
         res_net_layers = add_projection_block(res_net_layers, filters=64)
 
+        res_net_layers = add_identity_block(res_net_layers, filters=64)
+
         res_net_layers = add_projection_block(res_net_layers, filters=128)
         
         res_net_layers = add_projection_block(res_net_layers, filters=256)
