5685538976c50328b8db6d2530389b3214d687b3
Felipe-CF <prog.study.fcf@gmail.com>
2025-09-14 09:05:57 -0300

    feat: gerar gráficos

--- generate_grafics.py
+++ generate_grafics.py
@@ -1,19 +1,16 @@
+import os, json
 import matplotlib.pyplot as plt
-import re, os, json
-from graficos.metrics import Metrics
+from metrics_class import ModelMetrics
 
 
-
-def generate_grafics(test_regularization, params, epochs, label_line, figsize=(6, 12), colors = ['red', 'green', 'blue', 'orange']):
-    fit_metrics = Metrics(test_regularization)
-
+def generate_grafics(model, figsize=(6, 12), colors = ['red', 'green', 'blue', 'orange']):
     fig, axes = plt.subplots(nrows=3, ncols=2, figsize=figsize)
 
     axes = axes.ravel()
 
     plt.subplots_adjust(hspace=0.9, wspace=0.3, left=0.3) 
 
-    fig.suptitle('Histórico de Testes de Regularização', fontsize=18)
+    fig.suptitle('Histórico de Testes', fontsize=18)
 
     for i, param in enumerate(params):
         metrics = fit_metrics.get_metrics(param)
@@ -42,21 +39,27 @@ def generate_grafics(test_regularization, params, epochs, label_line, figsize=(6
     plt.show()
 
 
-if __name__ == "__main__":
-
+if __name__ == '__main__':
     file_dir = os.path.dirname(os.path.abspath(__file__))
 
-    history_path = os.path.join(file_dir, 'test_regularization.json')
+    checkpoint_dir = os.path.join(file_dir, 'screening_fit_history')
 
-    test_regularization = {}
+    history_path = os.path.join(checkpoint_dir, 'fit_history_auc_0.9166_val_auc_0.8587.json')
 
     with open(history_path, 'r') as file:
-        test_regularization = json.loads(file.read())
+        history = json.loads(file.read())
 
-    params = ["val_AUC","val_Accuracy", "val_F1_score", "val_Precision", "val_Recall", "val_loss"]
+    model_net = ModelMetrics(history)
 
-    label_line = ["Baseline","L1", "L2", "Dropout"] 
+    figsize=(6, 12)
 
-    epochs = test_regularization["L1"]["epoch"]
+    colors = ['red', 'green', 'blue', 'orange']
 
-    generate_grafics(test_regularization, params, epochs=epochs, figsize=(6, 18), label_line=label_line)
+    generate_grafics(
+        model=model_net, 
+        figsize=figsize, 
+        colors=colors
+        )
+
+
+    pass
---
+++ metrics_class.py
@@ -0,0 +1,64 @@
+
+
+
+class ModelMetrics:
+    def __init__(self, history):
+        self.__auc = (history['AUC'], history['val_AUC'])
+        self.__acc = (history['Accuracy'], history['val_Accuracy'])
+        self.__f1 = (history['F1_score'], history['val_F1_score'])
+        self.__prec = (history['Precision'], history['val_Precision'])
+        self.__rec = (history['Recall'], history['val_Recall'])
+        self.__loss = (history['loss'],history['val_loss'])
+        self.__lr = history['learning_rate']
+
+    def get_metrics(self, param=None):
+        if param == "AUC":
+            return self.__get_AUC()
+        
+        elif param == "Accuracy":
+            return self.__get_Accuracy()
+
+        elif param == "F1_score":
+            return self.__get_F1_score()
+
+        elif param == "Precision":
+            return self.__get_Precision()
+
+        elif param == "Recall":
+            return self.__get_Recall()
+        
+        elif param == "loss":
+            return self.__get_loss()
+        
+        elif param == "lr":
+            return self.__get_learning_rate()
+
+        else:
+            return self.__get_all()
+
+
+    def __get_AUC(self):
+        return self.__auc
+    
+    def __get_Accuracy(self):
+        return self.__acc
+    
+    def __get_F1_score(self):
+        return self.__f1
+    
+    def __get_Precision(self):
+        return self.__prec
+    
+    def __get_Recall(self):
+        return self.__rec
+    
+    def __get_loss(self):
+        return self.__loss
+    
+    def __get_learning_rate(self):
+        return self.__lr
+    
+    def __get_all(self):
+        return [self.__auc, self.__acc, self.__f1, self.__prec, self.__rec, self.__loss, self.__lr]
+
+
