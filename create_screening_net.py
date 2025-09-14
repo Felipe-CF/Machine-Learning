a38166aef22c9e93132700e8945f4a1ae59e9cb6
Felipe-CF <prog.study.fcf@gmail.com>
2025-09-14 00:12:01 -0300

    feat: ResNet20 com crescimento de camadas menos complexas

--- create_screening_net.py
+++ create_screening_net.py
@@ -37,7 +37,13 @@ def create_load_net(file_dir=None):
         
         res_net_layers = add_projection_block(res_net_layers, filters=256)
 
+        res_net_layers = add_identity_block(res_net_layers, filters=256)
+
         res_net_layers = add_projection_block(res_net_layers, filters=512)
+
+        res_net_layers = add_identity_block(res_net_layers, filters=512)
+        
+        res_net_layers = add_identity_block(res_net_layers, filters=512)
 
         res_net_layers = GlobalAveragePooling2D()(res_net_layers)
         
